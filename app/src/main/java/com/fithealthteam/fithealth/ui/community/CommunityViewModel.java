776d20840e97ab3aa3da07b25f4f6d7ba93882a4
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-08-02 23:07:40 +0800

    replace the existing empty activity project
    Replaced with bottom navigation bar project

---
+++ app/src/main/java/com/fithealthteam/fithealth/ui/community/CommunityViewModel.java
@@ -0,0 +1,7 @@
+package com.fithealthteam.fithealth.ui.community;
+
+import androidx.lifecycle.ViewModel;
+
+public class CommunityViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
