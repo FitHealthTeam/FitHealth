1c03ee34a81de8c25f1911da27f5d21d0c2c3812
JiannTsong <79738859+JiannTsong@users.noreply.github.com>
2021-08-11 00:14:35 +0800

    fix git issues

--- app/src/main/java/com/fithealthteam/fithealth/huawei/ui/community/CommunityViewModel.java
+++
@@ -1,8 +0,0 @@
-package com.fithealthteam.fithealth.huawei.ui.community;
-
-import androidx.lifecycle.ViewModel;
-
-public class CommunityViewModel extends ViewModel {
-
-    // TODO: Implement the ViewModel
-}
