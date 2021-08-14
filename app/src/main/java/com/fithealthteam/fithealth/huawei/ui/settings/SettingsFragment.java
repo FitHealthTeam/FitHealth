d42fb44da21791c70d656c0ae8b05f6f9cfb9dfc
haoyihyap <79739179+haoyihyap@users.noreply.github.com>
2021-08-14 23:38:09 +0800

    update

--- app/src/main/java/com/fithealthteam/fithealth/huawei/ui/settings/SettingsFragment.java
+++ app/src/main/java/com/fithealthteam/fithealth/huawei/ui/settings/SettingsFragment.java
@@ -2,6 +2,7 @@ package com.fithealthteam.fithealth.huawei.ui.settings;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,8 +12,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.fithealthteam.fithealth.huawei.R;
+import com.fithealthteam.fithealth.huawei.authentication.authenticateActivity;
+import com.huawei.agconnect.auth.AGConnectAuth;
 
 public class SettingsFragment extends Fragment {
 
@@ -25,9 +29,19 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.settings_fragment, container, false);
+        View v = inflater.inflate(R.layout.settings_fragment, container, false);
 
+        Button logoutBtn = v.findViewById(R.id.logoutBtn);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AGConnectAuth.getInstance().signOut();
+                Intent intent = new Intent(getContext(), authenticateActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        return v;
     }
 
     @Override
