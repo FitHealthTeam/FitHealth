5d8e3ccf20098345ae0a6d59f330c994cab5ad10
haoyihyap <79739179+haoyihyap@users.noreply.github.com>
2021-08-15 16:31:25 +0800

    push update

---
+++ app/src/main/java/com/fithealthteam/fithealth/huawei/authentication/passResetActivity.java
@@ -0,0 +1,16 @@
+package com.fithealthteam.fithealth.huawei.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fithealthteam.fithealth.huawei.R;
+
+public class passResetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pass_reset);
+    }
+}
