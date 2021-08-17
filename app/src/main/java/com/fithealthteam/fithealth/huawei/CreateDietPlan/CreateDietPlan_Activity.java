f8663e0cd06349a901962f94a51b3beec852848d
BastianChang <changlihao@1utar.my>
2021-08-17 18:14:21 +0800

    add CreateDietPlan

---
+++ app/src/main/java/com/fithealthteam/fithealth/huawei/CreateDietPlan/CreateDietPlan_Activity.java
@@ -0,0 +1,16 @@
+package com.fithealthteam.fithealth.huawei.CreateDietPlan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fithealthteam.fithealth.huawei.R;
+
+public class CreateDietPlan_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_diet_plan);
+    }
+}
