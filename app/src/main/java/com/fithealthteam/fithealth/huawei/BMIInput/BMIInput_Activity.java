5d5ce8c8d654eaeffd5c1e929d98ebf666eab80c
BastianChang <changlihao@1utar.my>
2021-08-19 02:49:30 +0800

    update bmi

--- app/src/main/java/com/fithealthteam/fithealth/huawei/BMIInput/BMIInput_Activity.java
+++ app/src/main/java/com/fithealthteam/fithealth/huawei/BMIInput/BMIInput_Activity.java
@@ -12,15 +12,19 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.fithealthteam.fithealth.huawei.CloudDB.CloudDBZoneWrapper;
+import com.fithealthteam.fithealth.huawei.CloudDB.exercise;
 import com.fithealthteam.fithealth.huawei.CloudDB.user;
 import com.fithealthteam.fithealth.huawei.MainActivity;
 import com.fithealthteam.fithealth.huawei.R;
 import com.fithealthteam.fithealth.huawei.authentication.authenticateActivity;
+import com.fithealthteam.fithealth.huawei.ui.settings.SettingsFragment;
 import com.huawei.agconnect.auth.AGConnectAuth;
 import com.huawei.agconnect.auth.AGConnectUser;
 import com.huawei.agconnect.cloud.database.CloudDBZoneQuery;
 
-public class BMIInput_Activity extends AppCompatActivity {
+import java.util.List;
+
+public class BMIInput_Activity extends AppCompatActivity implements CloudDBZoneWrapper.userUICallBack {
 
     Button btnSave;
     EditText inputWeight,inputHeight;
@@ -28,7 +32,7 @@ public class BMIInput_Activity extends AppCompatActivity {
     double weight;
     double height;
 
-    private Handler handler;
+    private Handler handler = new Handler();
     private CloudDBZoneWrapper cloudDBZoneWrapperInstance;
 
     AGConnectUser user = AGConnectAuth.getInstance().getCurrentUser();
@@ -45,10 +49,19 @@ public class BMIInput_Activity extends AppCompatActivity {
         inputWeight = findViewById(R.id.weight);
         inputHeight = findViewById(R.id.height);
 
+        //proceed to initialize cloudDBZoneWrapper
+        handler.post(()->{
+            initCloudDBZone();
+        });
+
+        //execute the cloudDB task, delayed a bit to wait for initialization complete
+        handler.post(()->{
+            queryAll();
+        });
+
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
 
                 // Get Text From User Input
                 if(inputWeight.getText().length() == 0 || inputHeight.length() == 0){
@@ -56,6 +69,10 @@ public class BMIInput_Activity extends AppCompatActivity {
                 }else{
                     weight = Double.parseDouble(String.valueOf(inputWeight.getText()));
                     height = Double.parseDouble(String.valueOf(inputHeight.getText()))/100;
+
+                    handler.post(()->{
+                        cloudDBZoneWrapperInstance.updateWeight(user.getUid(),weight,height);
+                    });
 
                     Toast.makeText(getApplicationContext(), "Weight: " + weight + "; Height: " + height , Toast.LENGTH_SHORT).show();
                     finish();
@@ -73,6 +90,43 @@ public class BMIInput_Activity extends AppCompatActivity {
         cloudDBZoneWrapperInstance.closeCloudDBZone();
     }
 
+    //initialize cloudDBZone
+    private void initCloudDBZone(){
+        handler.post(()->{
+            //add callback into cloudDBZoneWrapper
+            cloudDBZoneWrapperInstance.addUserCallBack(BMIInput_Activity.this);
+
+            //initialize
+            cloudDBZoneWrapperInstance.createObjectType();
+            cloudDBZoneWrapperInstance.openCloudDBZone();
+        });
+    }
+
+    public void queryAll(){
+        handler.postDelayed(()->{
+            CloudDBZoneQuery<user> query2 = CloudDBZoneQuery.where(user.class).equalTo("id",user.getUid());
+            cloudDBZoneWrapperInstance.queryUser(query2);
+        },500);
+    }
 
 
+    @Override
+    public void userOnAddorQuery(List<com.fithealthteam.fithealth.huawei.CloudDB.user> userList) {
+
+    }
+
+    @Override
+    public void userOnSubscribe(List<com.fithealthteam.fithealth.huawei.CloudDB.user> userList) {
+
+    }
+
+    @Override
+    public void userOnDelete(List<com.fithealthteam.fithealth.huawei.CloudDB.user> userList) {
+
+    }
+
+    @Override
+    public void userShowError(String error) {
+
+    }
 }
