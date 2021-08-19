f5d82b86bd2fc60017e18c5a095dc4bba1b2b965
haoyihyap <haoyihyap@1utar.my>
2021-08-19 23:21:13 +0800

    update auth

---
+++ app/src/main/java/com/fithealthteam/fithealth/huawei/Notification/backgroundProcess.java
@@ -0,0 +1,27 @@
+package com.fithealthteam.fithealth.huawei.Notification;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.fithealthteam.fithealth.huawei.R;
+
+public class backgroundProcess extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("MyAPP", "background is called");
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "FitHealth")
+                    .setSmallIcon(R.drawable.auth_fithealthlogo)
+                    .setVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
+                    .setContentTitle(intent.getStringExtra("pushTitle"))
+                    .setContentText(intent.getStringExtra("pushMessage"))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+            notificationManager.notify(200,builder.build());
+        }
+}
